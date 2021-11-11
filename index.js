053e317bb97b3ed703c42525f941a434dc15b80c
Jeongin Yoon <jy3524@nyu.edu>
2021-11-11 08:35:50 -0500

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 400
+const express = require('express');
+const app = express();
+const port = 4000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
